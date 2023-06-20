8b64286f31d864d3266caa1698475e6e395b127f
ThomasRetornaz <ThomasRetornaz@users.noreply.github.com>
2023-06-20 03:46:02 +0000

    deploy: 9fa2c41ae3e5593d8148870b32cfee7f88e2915c

---
+++ navtreedata.js
@@ -0,0 +1,121 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "E.V.E", "index.html", [
+    [ "The Expressive Vector Engine", "index.html", "index" ],
+    [ "Installation & Quick Start", "install.html", [
+      [ "Pre-requisites", "install.html#autotoc_md119", null ],
+      [ "Retrieving the source", "install.html#autotoc_md120", [
+        [ "Github", "install.html#autotoc_md121", null ],
+        [ "CPM", "install.html#autotoc_md122", null ],
+        [ "SPACK", "install.html#autotoc_md123", null ],
+        [ "AUR", "install.html#autotoc_md124", null ],
+        [ "Conan", "install.html#autotoc_md125", null ],
+        [ "VCPKG", "install.html#autotoc_md126", null ]
+      ] ],
+      [ "Installation from Source", "install.html#autotoc_md127", [
+        [ "Setting up the Library", "install.html#autotoc_md128", null ],
+        [ "Building the Documentation", "install.html#autotoc_md129", null ]
+      ] ],
+      [ "Using the library", "install.html#autotoc_md130", [
+        [ "Compilation", "install.html#autotoc_md131", null ],
+        [ "Instruction Set Selection", "install.html#autotoc_md132", null ],
+        [ "Execution", "install.html#autotoc_md133", null ],
+        [ "Use in CMake", "install.html#autotoc_md134", null ]
+      ] ],
+      [ "Advanced options", "install.html#autotoc_md135", null ]
+    ] ],
+    [ "Tutorials", "tutorials.html", "tutorials" ],
+    [ "Why and How", "eve-dev.html", "eve-dev" ],
+    [ "Change Log", "changelog.html", [
+      [ "Version 2023.02.15", "changelog.html#autotoc_md136", [
+        [ "What's Changed", "changelog.html#autotoc_md137", [
+          [ "Removal and Depreciation", "changelog.html#autotoc_md138", null ],
+          [ "Architectures/Compilers Support & Fixes", "changelog.html#autotoc_md139", [
+            [ "The One Big News for this release: SVE", "changelog.html#autotoc_md140", null ],
+            [ "Other Fixes", "changelog.html#autotoc_md141", null ]
+          ] ],
+          [ "Features", "changelog.html#autotoc_md142", null ],
+          [ "Bug Fixes", "changelog.html#autotoc_md143", null ]
+        ] ]
+      ] ],
+      [ "Version 2022.09.1", "changelog.html#autotoc_md144", null ],
+      [ "Version 2022.09.0", "changelog.html#autotoc_md145", [
+        [ "What's Changed", "changelog.html#autotoc_md146", null ],
+        [ "New Contributors", "changelog.html#autotoc_md147", null ]
+      ] ],
+      [ "Version 2022.03.0", "changelog.html#autotoc_md148", [
+        [ "What's Changed", "changelog.html#autotoc_md149", null ],
+        [ "New Contributors", "changelog.html#autotoc_md150", null ]
+      ] ],
+      [ "Version 2021.10.0", "changelog.html#autotoc_md151", [
+        [ "What's Changed", "changelog.html#autotoc_md152", null ]
+      ] ],
+      [ "Version beta.2020.09", "changelog.html#autotoc_md153", null ]
+    ] ],
+    [ "Licence", "licence.html", null ],
+    [ "Interaction with native code.", "inter-with-native.html", [
+      [ "What about non-native sizes?", "inter-with-native.html#autotoc_md160", null ]
+    ] ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", "namespacemembers_vars" ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Concepts", "concepts.html", "concepts" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"algo_rationale.html",
+"group__conditional_ga6b75905a0b2d6320cadb973c752a8cd1.html#ga6b75905a0b2d6320cadb973c752a8cd1",
+"group__core__predicates_gad8c0f5b1fcc6893e05d6460f7846006e.html#gad8c0f5b1fcc6893e05d6460f7846006e",
+"group__polynomial_gaa4853888b0e6be1fdbc24c9f6b372599.html#gaa4853888b0e6be1fdbc24c9f6b372599",
+"structeve_1_1if__.html#af4da25e9482c78b71639ae7a8d9bd282",
+"structrelaxed__iterator.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex5.js
@@ -0,0 +1,11 @@
+var NAVTREEINDEX5 =
+{
+"structrelaxed__iterator.html":[7,0,0,2,1],
+"structrelaxed__range.html":[7,0,0,2,3],
+"structrelaxed__sentinel__for.html":[7,0,0,2,2],
+"structreverse__iterator.html":[7,0,0,3,18],
+"structreverse__range.html":[7,0,0,3,19],
+"structzip__iterator.html":[7,0,0,3,20],
+"structzip__range.html":[7,0,0,3,21],
+"tutorials.html":[2]
+};
